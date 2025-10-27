995969d9a2545b8d82f76c9e631961957d9ae9dc
Sam Silva <64173917+solidshadw@users.noreply.github.com>
2025-10-26 18:36:48 -0600

    Remove Giscus configuration from astro.config.ts
    Removed Giscus configuration and related environment variables.

--- astro.config.ts
+++ astro.config.ts
@@ -9,18 +9,6 @@ import spectre from './package/src';
 import node from '@astrojs/node';
 import { spectreDark } from './src/ec-theme';
 
-const {
-  GISCUS_REPO,
-  GISCUS_REPO_ID,
-  GISCUS_CATEGORY,
-  GISCUS_CATEGORY_ID,
-  GISCUS_MAPPING,
-  GISCUS_STRICT,
-  GISCUS_REACTIONS_ENABLED,
-  GISCUS_EMIT_METADATA,
-  GISCUS_LANG
-} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
-
 // https://astro.build/config
 const config = defineConfig({
   site: 'https://spectre.lou.gg',
@@ -46,17 +34,6 @@ const config = defineConfig({
           title: 'Projects'
         }
       },
-      giscus: {
-        repository: GISCUS_REPO,
-        repositoryId: GISCUS_REPO_ID,
-        category: GISCUS_CATEGORY,
-        categoryId: GISCUS_CATEGORY_ID,
-        mapping: GISCUS_MAPPING as any,
-        strict: GISCUS_STRICT === "true",
-        reactionsEnabled: GISCUS_REACTIONS_ENABLED === "true",
-        emitMetadata: GISCUS_EMIT_METADATA === "true",
-        lang: GISCUS_LANG,
-      }
     })
   ],
   adapter: node({
